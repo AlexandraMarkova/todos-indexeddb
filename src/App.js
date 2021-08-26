a90cb866e7ef1af9b1f87c13ea645e63f9c36eb0
AlexandraMarkova <a.markova@ukr.net>
2021-08-26 22:12:43 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <h1>Welcome to your To Do list</h1>
+        </div>
+      </header>
+      <main>{/* <Main /> */}</main>
+    </div>
+  );
+}
+
+export default App;
